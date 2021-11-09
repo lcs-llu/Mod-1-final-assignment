f0c15284ba3932ca675f9ec26871e5a1dedac073
Leo Lu <llu@lcs.on.ca>
2021-11-09 14:52:07 -0500

    Initial Commit

---
+++ Mod 1 final assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mod 1 final assignment
+//
+//  Created by Leo Lu on 2021-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mod 1 final assignment/Mod_1_final_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod_1_final_assignmentApp.swift
+//  Mod 1 final assignment
+//
+//  Created by Leo Lu on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct Mod_1_final_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
