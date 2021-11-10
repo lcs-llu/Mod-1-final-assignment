e62b7eedaa1b5933616a9347e5a04bb125b96696
Leo Lu <llu@lcs.on.ca>
2021-11-09 19:24:42 -0500

    trade card 1

--- Mod 1 final assignment/ContentView.swift
+++ Mod 1 final assignment/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            
+            ZStack{
+                
+              Image("mancity")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 1500, height: 500)
+                
+                Image("aguero")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 450)
+                
+                VStack{
+                    
+                    Text("93:20")
+                    Text("Manchester City")
+                    
+                    Text("Kun Aguero")
+                    Text("club legends")
+                    
+                    Text("Born in  June 2 1988")
+                        .padding(.top, 300)
+                    Text("age: 33")
+                    Text("height: 1.73m")
+                    Text("carrer start: 1997")
+                    Text("Salary: 11.97 million GBP")
+                    
+                }
+                
+                
+            }
+        }
     }
 }
 
